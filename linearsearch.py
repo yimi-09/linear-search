526d42e94f8382e9b386e0dc54bbd07a19d26919
yimi-09 <of05267648@priestley.ac.uk>
2025-11-13 13:52:51 +0000

    Add files via upload

---
+++ linearsearch.py
@@ -0,0 +1,15 @@
+nlist=[2, 4, 6, 8, 9, 12, 15]
+search_Term = input("Enter a number: ")
+for x in range (len(nlist)):
+    
+    if (search_Term == nlist[x]):
+
+        found == True
+
+if (found == True):
+    print("found")
+
+else:
+    print("not found")
+
+    
